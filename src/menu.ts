d03b807d1b16bd7e882e80f5ce5b69949d4717af
yuxincxoi <yjyj0774@gmail.com>
2024-07-04 11:06:12 +0900

    함수 return type 설정

--- src/menu.ts
+++ src/menu.ts
@@ -1,11 +1,11 @@
 const root = document.getElementById('root')!;
 
-const anchorTags = () => {
+const anchorTags = () : string => {
   let result = `<a href="#">메뉴</a>`
   return result;
 }
 
-const liTags = (children : string) => {
+const liTags = (children : string) : string => {
   let result = `<li>${children}</li>`;
   return result;
 }
