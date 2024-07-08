318ed4e3501bc2c85f885e4036d40281e12ceeec
yuxincxoi <yjyj0774@gmail.com>
2024-07-08 10:21:36 +0900

    babel build

---
+++ calculator_ver2/dist/app.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var _app = _interopRequireDefault(require("./app.module"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+(0, _app["default"])();
